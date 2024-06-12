c574ad196b4528580d1dccf4f38d7e3696abaf71
Aris Karagiannidis <arkaragian@gmail.com>
2024-06-12 23:49:46 +0300

    refactor: src gen for p/inv marshalling at compile

--- Bindings/TSCursor.cs
+++ Bindings/TSCursor.cs
@@ -1,7 +1,7 @@
 using System.Runtime.InteropServices;
 
 namespace TreeSitter.CSharp;
-public sealed class TSCursor : IDisposable {
+public sealed partial class TSCursor : IDisposable {
     [StructLayout(LayoutKind.Sequential)]
     public struct TSTreeCursor {
         private IntPtr Tree;
@@ -50,93 +50,107 @@ public sealed class TSCursor : IDisposable {
     public TSCursor copy() { return new TSCursor(ts_tree_cursor_copy(ref cursor), lang); }
 
     #region PInvoke
-    /**
-    * Create a new tree cursor starting from the given node.
-    *
-    * A tree cursor allows you to walk a syntax tree more efficiently than is
-    * possible using the `TSNode` functions. It is a mutable object that is always
-    * on a certain syntax node, and can be moved imperatively to different nodes.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern TSTreeCursor ts_tree_cursor_new(TSNode node);
+    /// <summary>
+    /// Create a new tree cursor starting from the given node.
+    /// A tree cursor allows you to walk a syntax tree more efficiently than is
+    /// possible using the `TSNode` functions. It is a mutable object that is always
+    /// on a certain syntax node, and can be moved imperatively to different nodes.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial TSTreeCursor ts_tree_cursor_new(TSNode node);
 
-    /**
-    * Delete a tree cursor, freeing all of the memory that it used.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern void ts_tree_cursor_delete(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Delete a tree cursor, freeing all of the memory that it used.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial void ts_tree_cursor_delete(ref TSTreeCursor cursor);
 
-    /**
-    * Re-initialize a tree cursor to start at a different node.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern void ts_tree_cursor_reset(ref TSTreeCursor cursor, TSNode node);
+    /// <summary>
+    /// Re-initialize a tree cursor to start at a different node.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial void ts_tree_cursor_reset(ref TSTreeCursor cursor, TSNode node);
 
-    /**
-    * Get the tree cursor's current node.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern TSNode ts_tree_cursor_current_node(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Get the tree cursor's current node.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial TSNode ts_tree_cursor_current_node(ref TSTreeCursor cursor);
 
-    /**
-    * Get the field name of the tree cursor's current node.
-    *
-    * This returns `NULL` if the current node doesn't have a field.
-    * See also `ts_node_child_by_field_name`.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern IntPtr ts_tree_cursor_current_field_name(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Get the field name of the tree cursor's current node.
+    /// This returns `NULL` if the current node doesn't have a field.
+    /// See also `ts_node_child_by_field_name`.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial IntPtr ts_tree_cursor_current_field_name(ref TSTreeCursor cursor);
 
-    /**
-    * Get the field id of the tree cursor's current node.
-    *
-    * This returns zero if the current node doesn't have a field.
-    * See also `ts_node_child_by_field_id`, `ts_language_field_id_for_name`.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern ushort ts_tree_cursor_current_field_id(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Get the field id of the tree cursor's current node.
+    /// This returns zero if the current node doesn't have a field.
+    /// See also `ts_node_child_by_field_id`, `ts_language_field_id_for_name`.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial ushort ts_tree_cursor_current_field_id(ref TSTreeCursor cursor);
 
-    /**
-    * Move the cursor to the parent of its current node.
-    *
-    * This returns `true` if the cursor successfully moved, and returns `false`
-    * if there was no parent node (the cursor was already on the root node).
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool ts_tree_cursor_goto_parent(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Move the cursor to the parent of its current node.
+    /// This returns `true` if the cursor successfully moved, and returns `false`
+    /// if there was no parent node (the cursor was already on the root node).
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool ts_tree_cursor_goto_parent(ref TSTreeCursor cursor);
 
-    /**
-    * Move the cursor to the next sibling of its current node.
-    *
-    * This returns `true` if the cursor successfully moved, and returns `false`
-    * if there was no next sibling node.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool ts_tree_cursor_goto_next_sibling(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Move the cursor to the next sibling of its current node.
+    /// This returns `true` if the cursor successfully moved, and returns `false`
+    /// if there was no next sibling node.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool ts_tree_cursor_goto_next_sibling(ref TSTreeCursor cursor);
 
-    /**
-    * Move the cursor to the first child of its current node.
-    *
-    * This returns `true` if the cursor successfully moved, and returns `false`
-    * if there were no children.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool ts_tree_cursor_goto_first_child(ref TSTreeCursor cursor);
+    /// <summary>
+    /// Move the cursor to the first child of its current node.
+    /// This returns `true` if the cursor successfully moved, and returns `false`
+    /// if there were no children.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool ts_tree_cursor_goto_first_child(ref TSTreeCursor cursor);
 
-    /**
-    * Move the cursor to the first child of its current node that extends beyond
-    * the given byte offset or point.
-    *
-    * This returns the index of the child node if one was found, and returns -1
-    * if no such child was found.
-    */
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern long ts_tree_cursor_goto_first_child_for_byte(ref TSTreeCursor cursor, uint byteOffset);
+    /// <summary>
+    /// Move the cursor to the first child of its current node that extends beyond
+    /// the given byte offset
+    /// This returns the index of the child node if one was found, and returns -1
+    /// if no such child was found.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial long ts_tree_cursor_goto_first_child_for_byte(ref TSTreeCursor cursor, uint byteOffset);
 
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern long ts_tree_cursor_goto_first_child_for_point(ref TSTreeCursor cursor, TSPoint point);
+    /// <summary>
+    /// Move the cursor to the first child of its current node that extends beyond
+    /// the given point.
+    /// This returns the index of the child node if one was found, and returns -1
+    /// if no such child was found.
+    /// </summary>
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial long ts_tree_cursor_goto_first_child_for_point(ref TSTreeCursor cursor, TSPoint point);
 
-    [DllImport("tree-sitter.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern TSTreeCursor ts_tree_cursor_copy(ref TSTreeCursor cursor);
+    [LibraryImport("tree-sitter.dll")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial TSTreeCursor ts_tree_cursor_copy(ref TSTreeCursor cursor);
     #endregion
 }
